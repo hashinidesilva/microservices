407a78bb0e2275e83c7dfc46bcfb6d2aa71a5b73
hashinidesilva <hashmdesilva@gmail.com>
2022-04-16 01:46:53 +0530

    Initial commit

---
+++ movie-info-service/src/test/java/com/hashini/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashini.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
