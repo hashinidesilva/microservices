f1d0c3b7c33710564327d2b1f560f0e44e20e468
hashinidesilva <hashmdesilva@gmail.com>
2022-04-20 03:30:33 +0530

    Adding Eureka server and clients

--- ratings-data-service/src/main/java/com/hashini/ratingsdataservice/RatingsDataServiceApplication.java
+++ ratings-data-service/src/main/java/com/hashini/ratingsdataservice/RatingsDataServiceApplication.java
@@ -2,12 +2,14 @@ package com.hashini.ratingsdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingsDataServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RatingsDataServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RatingsDataServiceApplication.class, args);
+    }
 
 }
