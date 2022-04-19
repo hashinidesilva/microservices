35867ec4233ce25060c171330753ad46ff2dfe2b
hashinidesilva <hashmdesilva@gmail.com>
2022-04-20 01:28:13 +0530

    Using WebClient to call external microservice API

--- movie-catalog-service/src/main/java/com/hashini/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/hashini/moviecatalogservice/resources/MovieCatalogResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,22 +20,33 @@ import java.util.stream.Collectors;
 public class MovieCatalogResource {
 
     private final RestTemplate restTemplate;
+    private final WebClient.Builder webClientBuilder;
 
     @Autowired
-    public MovieCatalogResource(RestTemplate restTemplate) {
+    public MovieCatalogResource(RestTemplate restTemplate,
+                                WebClient.Builder webClientBuilder) {
         this.restTemplate = restTemplate;
+        this.webClientBuilder = webClientBuilder;
     }
 
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable String userId) {
+
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234", 4),
                 new Rating("5678", 3)
         );
 
         return ratings.stream().map(rating -> {
-                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(),
-                            Movie.class);
+//                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(),
+//                            Movie.class);
+                    Movie movie = webClientBuilder.build()
+                            .get()
+                            .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                            .retrieve()
+                            .bodyToMono(Movie.class)
+                            .block();
+
                     return new CatalogItem(movie.getName(), "Description1", rating.getRating());
                 })
                 .collect(Collectors.toList());
