a70e12f888eb2acf32b23864a5641ff11f1c2749
hashinidesilva <hashmdesilva@gmail.com>
2022-04-20 01:55:11 +0530

    Making a call to the Ratings API

--- movie-catalog-service/src/main/java/com/hashini/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/hashini/moviecatalogservice/resources/MovieCatalogResource.java
@@ -2,7 +2,7 @@ package com.hashini.moviecatalogservice.resources;
 
 import com.hashini.moviecatalogservice.models.CatalogItem;
 import com.hashini.moviecatalogservice.models.Movie;
-import com.hashini.moviecatalogservice.models.Rating;
+import com.hashini.moviecatalogservice.models.UserRating;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,23 +31,25 @@ public class MovieCatalogResource {
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable String userId) {
 
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("5678", 3)
-        );
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId,
+                UserRating.class);
 
-        return ratings.stream().map(rating -> {
-//                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(),
-//                            Movie.class);
-                    Movie movie = webClientBuilder.build()
-                            .get()
-                            .uri("http://localhost:8082/movies/" + rating.getMovieId())
-                            .retrieve()
-                            .bodyToMono(Movie.class)
-                            .block();
+        return ratings.getUserRating().stream().map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(),
+                            Movie.class);
 
                     return new CatalogItem(movie.getName(), "Description1", rating.getRating());
                 })
                 .collect(Collectors.toList());
     }
 }
+
+
+                    /*
+                    Movie movie = webClientBuilder.build()
+                            .get()
+                            .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                            .retrieve()
+                            .bodyToMono(Movie.class)
+                            .block();
+                    */
--- ratings-data-service/src/main/java/com/hashini/ratingsdataservice/resources/RatingsDataResource.java
+++ ratings-data-service/src/main/java/com/hashini/ratingsdataservice/resources/RatingsDataResource.java
@@ -1,10 +1,14 @@
 package com.hashini.ratingsdataservice.resources;
 
 import com.hashini.ratingsdataservice.models.Rating;
+import com.hashini.ratingsdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -13,6 +17,15 @@ public class RatingsDataResource {
     @GetMapping("/{movieId}")
     public Rating getRating(@PathVariable String movieId) {
         return new Rating(movieId, 2);
+    }
+
+    @GetMapping("users/{userId}")
+    public UserRating getUserRatings(@PathVariable String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 2),
+                new Rating("5678", 2)
+        );
+        return new UserRating(ratings);
     }
 
 }
